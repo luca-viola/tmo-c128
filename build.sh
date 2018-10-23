188cc1a3e10eb65f9468a56f1db060198bd25dc8
lviola <luca.viola@lastminute.com>
2018-10-23 17:11:04 +0200

    fixed build of a .bin file, added d64 image

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 CC65_HOME=$HOME/cc65
 
-$CC65_HOME/bin/cl65 -o seq_burst --start-addr $1300 -t c128 -C c128-asm.cfg seq_burst.s
+$CC65_HOME/bin/cl65 -o seq_burst.bin --start-addr $1300 -t c128 -C c128-asm.cfg seq_burst.s
